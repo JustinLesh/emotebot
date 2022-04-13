8180c9d1cd567847f8631042cdf3151df30c4dd4
Justin Lesh <jlesh56021@gmail.com>
2022-04-12 22:45:44 -0500

    Remove functionallity half completed. TODO: voting

--- bot.py
+++ bot.py
@@ -1,22 +1,38 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+#client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix='/')
+
+@bot.command()
+async def add(ctx, url, name = None):
+    await ctx.channel.send(url + " " + name)
+
+@add.error
+async def add_error(ctx, error):
+    await ctx.channel.send(error)
+
+# @bot.command()
+# async def removeold(ctx, emoji: discord.Emoji):
+#     await ctx.channel.send("Deleted the emoji: " + "<:" + emoji.name + ":" + str(emoji.id) + ">")
+#     #await emoji.delete()
+
+@bot.command()
+async def remove(ctx, emoji: discord.Emoji):
+    #msg = await ctx.reply("Deleted the emoji: " + "<:" + emoji.name + ":" + str(emoji.id) + ">")
+    msg = await ctx.channel.send("React with ✅ or ❎ to vote to remove the emoji: " + "<:" + emoji.name + ":" + str(emoji.id) + ">", reference=ctx.message)
+    await msg.add_reaction("✅")
+    await msg.add_reaction("❎")
+    #await emoji.delete()
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('/addemote'):
-        await message.channel.send('Hello!')
-
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
